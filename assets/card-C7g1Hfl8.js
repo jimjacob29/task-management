bd7308d499bfbbb6f0aee3c864b90b9002e84cd9
Jimmy Jacob <jimmyjacob@Jimmys-MacBook-Pro.local>
2025-07-30 14:47:22 +0530

    Updates

--- assets/card-C7g1Hfl8.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D-SJopeZ.js";const e=({children:s,className:r="",...o})=>t.jsx("div",{...o,className:`${r} w-full rounded-[8px] bg-white p-3 shadow-sm`,children:s});export{e as C};
