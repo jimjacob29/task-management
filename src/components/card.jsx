cb723bdd661201a6676176594e937b1bcf30b41d
Jimmy Jacob <jimmyjacob@Jimmys-MacBook-Pro.local>
2025-07-31 15:31:37 +0530

    hoisting

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 const Card = ({ children, className = "", ...props }) => {
     return (
         <div {...props} className={`${className} w-full rounded-[8px] bg-white p-3 shadow-sm`}>
@@ -6,4 +8,4 @@ const Card = ({ children, className = "", ...props }) => {
     );
 };
 
-export default Card;
+export default memo(Card);
